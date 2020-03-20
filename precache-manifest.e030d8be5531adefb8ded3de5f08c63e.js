6b8d3baa2570fd54fc021f60f4de6457bdb157dd
Unai Bolivar <ujbolivar@gmail.com>
2020-03-20 19:26:28 +0100

    Updates

---
+++ precache-manifest.e030d8be5531adefb8ded3de5f08c63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6affabe1b3db27005882f8b7ab2656de",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b64c72b3b1e44fe8a730",
+    "url": "/monsters-rolodex/static/css/main.8502e0fb.chunk.css"
+  },
+  {
+    "revision": "570f35dee691a275e91e",
+    "url": "/monsters-rolodex/static/js/2.b293cc3b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.b293cc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64c72b3b1e44fe8a730",
+    "url": "/monsters-rolodex/static/js/main.b7e85815.chunk.js"
+  },
+  {
+    "revision": "b02e5c9b96f2d5be9776",
+    "url": "/monsters-rolodex/static/js/runtime-main.f50a04fe.js"
+  }
+]);
